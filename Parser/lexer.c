ad363a47d6bf415b0347538bbfab632e4972bb4c
Zyphicx <zyphicxs@gmail.com>
2016-11-11 18:48:58 +0200

    Modified lexer

--- Parser/lexer.c
+++ Parser/lexer.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 #include "token.h"
 #define MAX_LINE 512
 
 void lex(FILE *file, Token_List *list);
-Token *get_next_token(char *line);
-int get_next_word(char *s, char *target, int max);
+char *get_next_token(char *line, Token *token);
+int read_token(char *s, char *t, int len);
+int is_word(char *s);
+
+char *keywords[] = {"for",
+			"if",
+			"while"};
 
 int main(int argc, char *argv[]){
 	Token_List list;
@@ -26,39 +31,56 @@ int main(int argc, char *argv[]){
 void lex(FILE *file, Token_List *list){
 	int linecount = 0;
 	char line[MAX_LINE]; //It will end with a \0, so it won't read from old lines
-	while(1){
-		if(fgets(line, MAX_LINE - 1, file) == NULL){
-			fprintf(stderr, "Error with reading line");
-			exit(1);
-		}
-		/*printf("%s\n", line);
-		get_next_token(line);*/
+	char *line_ptr;
+	while(fgets(line, MAX_LINE - 1, file) != NULL){
+		line_ptr = line;
+		Token *token;
 
+		while(*line_ptr)
+			line_ptr = get_next_token(line_ptr, token);
 	}
 }
 
-Token *get_next_token(char *line){
-	Token token;
-	token.value = malloc(MAX_TOKEN_VALUE * sizeof(char));
+char *get_next_token(char *line, Token *token){
+	token = (Token *)malloc(sizeof(Token));
+
+	//Check for the following and set token type
 
 	//Check for words
-	if(get_next_word(line, token.value, MAX_TOKEN_VALUE)){
-		printf("%s", token.value);
+	int length;
+
+	if(length = is_word(line)){
+		token->value = (char *)malloc(length * sizeof(char) + 1); //Add 1 for null character
+		read_token(line, token->value, length);
+		line += length + 1;
+		printf("The word is \"%s\", and it is of length: %d\n", token->value, length);
 	}
 
 	//Check for numbers
 
 	//Check for specific characters
+
+	//Malloc token char and set value
+	return line;
 }
 
-int get_next_word(char *s, char *target, int max){ //USE STRTOK OTHERWISE!!!!!!!!
-	while(isspace(*s++))
-		;
-	int count = 1;
-	if(!isalpha(*target++ = *s++))
+int read_token(char *s, char *t, int len){
+	while(isspace(*s))
+		++s;
+	while(len-- > 0){
+		*t++ = *s++;
+	}
+	*++t = '\0';
+}
+
+int is_word(char *s){
+	while(isspace(*s))
+		++s;
+	int count = 0;
+	if(!isalpha(*s++))
 		return 0;
-	while(isalnum(*target++ = *s++) && --max > 1)
-		count++;
-	*target = '\0';
-	return count + 1;
+	++count;
+	while(isalnum(*s++))
+		++count;
+	return count;
 }
--- Parser/token.h
+++ Parser/token.h
@@ -1,11 +1,12 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
 #include <stddef.h>
 #define MAX_TOKEN_VALUE 32
 
-enum token_type {IF, WHILE, L_PAREN, R_PAREN, IDENTIFIER};
+extern char *keywords[];
 
-char *keywords[] = {"for", //USE A HEADER GUARD
-                    "if",
-                    "while"};
+enum token_type {IF, WHILE, L_PAREN, R_PAREN, IDENTIFIER};
 
 struct token{
 	enum token_type type;
@@ -24,4 +25,4 @@ typedef struct token_list Token_List;
 
 void new_list(Token_List *list, size_t init_size);
 void add_entry(Token_List *list, Token token);
-    
+#endif
